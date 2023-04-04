6f10f958c264a19b4f9f80fe72b43cd835d69455
Justin Reichelt <justin.j.reichelt@gmail.com>
2023-04-04 11:10:58 -0400

    edited switch command and researching ternary operator

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,53 +1,79 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+let firstName = 'Justin';
+console.log('Justin');
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Reichelt';
+console.log('Reichelt');
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+let fullName = 'Justin Reichelt';
 // 4 - Console log the value of `fullName`
-
+console.log('Justin Reichelt');
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-
+let luckyNumber = 55;
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
-
+console.log('My name is '+ fullName + ' and I think ' + luckyNumber +' is a winner!');
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food = ('vietnamese ' + 'pho');
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 1;
+sentence = 'pets total 1';
+console.log('pets total 1');
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 3;
 // 11 - Add two pets to your `pets` variable
-
+pets+= 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+let allowedPets = 5;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+if(adventurous = true){console.log('Adventures are great!');}else {console.log('How about we stay home?')}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-
+if(luckyNumber = 2 || adventurous === true){console.log('Roll the dice!');}
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
+pets = 3;
+console.log('number of pets changed to 3');
+allowedPets = 2;
+console.log('allowed pets changed to 2');
+if(pets < allowedPets){console.log('I can have more pets!')}else{console.log('Oh no, I have too many pets!')}
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-
+if(pets = allowedPets){console.log('I have enough pets');}else if(pets > allowedPets){console.log('Oh no, I have too many pets!');}
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
+let mostPets = 3;
+if(pets === 'mostPets'){console.log('mostPets');}
+else if(friendsPets === 'mostPets'){console.log('mostPets');}
+console.log('mostPets');
 // correctly checks the `pets` and `friendsPets` variables, and
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
 
 // 17 - Write a *switch* statement that logs:
+
+let LuckyNumbers = 1;
+switch(LuckyNumbers) {
+    case 1: LuckyNumbers = 1;console.log('First is the worst');
+    break;
+    case 2: LuckyNumbers = 2;console.log('Second is the best');
+    break;
+    case 3: LuckyNumbers = 3; console.log('Third is the one with the polka dot dress');
+    break;
+    default: console.log('Luck is what happens when preparation meets opportunity');
+}
+
+
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
 //      "Third is the one with the polka dot dress" if your lucky number is 3
@@ -55,5 +81,4 @@
 //      You'll need to research how to use switch statements!
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-
 
